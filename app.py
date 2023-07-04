05adc5523d2a234df1183cbcda989c3ca5ff304c
Saamiya96 <saamiyayousuf@hotmail.co.uk>
2023-07-04 14:36:47 +0100

    added relevant files to project

---
+++ app.py
@@ -0,0 +1,14 @@
+import os
+from flask import Flask, request
+
+# Create a new Flask app
+app = Flask(__name__)
+
+# == Your Routes Here ==
+
+
+# These lines start the server if you run this file directly
+# They also start the server configured to use the test database
+# if started in test mode.
+if __name__ == '__main__':
+    app.run(debug=True, port=int(os.environ.get('PORT', 5000)))
