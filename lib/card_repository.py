17cd631cb5f6767dcd9187316e3816dcb2edc8f8
Saamiya96 <saamiyayousuf@hotmail.co.uk>
2023-07-05 14:18:06 +0100

    completed tests

--- lib/card_repository.py
+++ lib/card_repository.py
@@ -11,61 +11,61 @@ class CardRepository:
             item = Card(row["id"], row["language_name"], row["age"], row["av_salary"], row["downloads"], row["popularity"], row["job_availability"])
             cards.append(item)
         return cards
-    
+     
     def find_by_language_name(self, language_name):
         rows = self._connection.execute(
             'SELECT * from cards WHERE language_name = %s', [language_name])
         if not rows:
             raise Exception("Coding language not listed, please try again.")
-        cards = []
-        for row in rows:
-             item = Card(row["id"], row["language_name"], row["age"], row["av_salary"], row["downloads"], row["popularity"], row["job_availability"])
-             cards.append(item)
-        return cards
+        else:
+            card = rows[0]
+        return Card(
+            card["id"], card["language_name"], card["age"], card["av_salary"], card["downloads"], card["popularity"], card["job_availability"]
+        )
     
     def find_by_age(self, age):
         rows = self._connection.execute(
             'SELECT * from cards WHERE age = %s', [age])
         if not rows:
             raise Exception("Coding age not listed, please try again.")
-        cards = []
-        for row in rows:
-             item = Card(row["id"], row["language_name"], row["age"], row["av_salary"], row["downloads"], row["popularity"], row["job_availability"])
-             cards.append(item)
-        return cards
+        else:
+            card = rows[0]
+        return Card(
+            card["id"], card["language_name"], card["age"], card["av_salary"], card["downloads"], card["popularity"], card["job_availability"]
+        )
     
     def find_by_av_salary(self, av_salary):
         rows = self._connection.execute(
             'SELECT * from cards WHERE av_salary = %s', [av_salary])
         if not rows:
             raise Exception("Average salary not listed, please try again.")
-        cards = []
-        for row in rows:
-             item = Card(row["id"], row["language_name"], row["age"], row["av_salary"], row["downloads"], row["popularity"], row["job_availability"])
-             cards.append(item)
-        return cards
+        else:
+            card = rows[0]
+        return Card(
+            card["id"], card["language_name"], card["age"], card["av_salary"], card["downloads"], card["popularity"], card["job_availability"]
+        )
     
     def find_by_downloads(self, downloads):
         rows = self._connection.execute(
             'SELECT * from cards WHERE downloads = %s', [downloads])
         if not rows:
             raise Exception("Download not listed, please try again.")
-        cards = []
-        for row in rows:
-             item = Card(row["id"], row["language_name"], row["age"], row["av_salary"], row["downloads"], row["popularity"], row["job_availability"])
-             cards.append(item)
-        return cards
+        else:
+            card = rows[0]
+        return Card(
+            card["id"], card["language_name"], card["age"], card["av_salary"], card["downloads"], card["popularity"], card["job_availability"]
+        )
     
     def find_by_popularity(self, popularity):
         rows = self._connection.execute(
             'SELECT * from cards WHERE popularity = %s', [popularity])
         if not rows:
             raise Exception("Popularity not listed, please try again.")
-        cards = []
-        for row in rows:
-             item = Card(row["id"], row["language_name"], row["age"], row["av_salary"], row["downloads"], row["popularity"], row["job_availability"])
-             cards.append(item)
-        return cards
+        else:
+            card = rows[0]
+        return Card(
+            card["id"], card["language_name"], card["age"], card["av_salary"], card["downloads"], card["popularity"], card["job_availability"]
+        )
     
     def find_by_job_availability(self, job_availability):
         rows = self._connection.execute(
