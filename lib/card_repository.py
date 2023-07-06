0ad09dbedcdcad5913e5f5737f070ac322c022fe
Adrian Hards <github@ahards.com>
2023-07-06 15:16:05 +0100

    fix testing issues

--- lib/card_repository.py
+++ lib/card_repository.py
@@ -1,96 +1,147 @@
-from card import Card
+from lib.card import Card
 import requests
+
 
 class CardRepository:
     def __init__(self, connection, requester) -> None:
         self._connection = connection
         self.requester = requester
-        
+
     def all(self):
-        rows = self._connection.execute('SELECT * from cards ORDER BY id')
+        rows = self._connection.execute("SELECT * from cards ORDER BY id")
         cards = []
         for row in rows:
-            item = Card(row["id"], row["language_name"], row["age"], row["av_salary"], row["downloads"], row["popularity"], row["job_availability"])
+            item = Card(
+                row["id"],
+                row["language_name"],
+                row["age"],
+                row["av_salary"],
+                row["downloads"],
+                row["popularity"],
+                row["job_availability"],
+            )
             cards.append(item)
         return cards
-    
+
     def find_by_language_name(self, language_name):
         rows = self._connection.execute(
-            'SELECT * from cards WHERE language_name = %s', [language_name])
+            "SELECT * from cards WHERE language_name = %s", [language_name]
+        )
         if not rows:
             raise Exception("Coding language not listed, please try again.")
         else:
             card = rows[0]
         return Card(
-            card["id"], card["language_name"], card["age"], card["av_salary"], card["downloads"], card["popularity"], card["job_availability"]
+            card["id"],
+            card["language_name"],
+            card["age"],
+            card["av_salary"],
+            card["downloads"],
+            card["popularity"],
+            card["job_availability"],
         )
-    
+
     def find_by_age(self, age):
-        rows = self._connection.execute(
-            'SELECT * from cards WHERE age = %s', [age])
+        rows = self._connection.execute("SELECT * from cards WHERE age = %s", [age])
         if not rows:
             raise Exception("Coding age not listed, please try again.")
         else:
             card = rows[0]
         return Card(
-            card["id"], card["language_name"], card["age"], card["av_salary"], card["downloads"], card["popularity"], card["job_availability"]
+            card["id"],
+            card["language_name"],
+            card["age"],
+            card["av_salary"],
+            card["downloads"],
+            card["popularity"],
+            card["job_availability"],
         )
-    
+
     def find_by_av_salary(self, av_salary):
         rows = self._connection.execute(
-            'SELECT * from cards WHERE av_salary = %s', [av_salary])
+            "SELECT * from cards WHERE av_salary = %s", [av_salary]
+        )
         if not rows:
             raise Exception("Average salary not listed, please try again.")
         else:
             card = rows[0]
         return Card(
-            card["id"], card["language_name"], card["age"], card["av_salary"], card["downloads"], card["popularity"], card["job_availability"]
+            card["id"],
+            card["language_name"],
+            card["age"],
+            card["av_salary"],
+            card["downloads"],
+            card["popularity"],
+            card["job_availability"],
         )
-    
+
     def find_by_downloads(self, downloads):
         rows = self._connection.execute(
-            'SELECT * from cards WHERE downloads = %s', [downloads])
+            "SELECT * from cards WHERE downloads = %s", [downloads]
+        )
         if not rows:
             raise Exception("Download not listed, please try again.")
         else:
             card = rows[0]
         return Card(
-            card["id"], card["language_name"], card["age"], card["av_salary"], card["downloads"], card["popularity"], card["job_availability"]
+            card["id"],
+            card["language_name"],
+            card["age"],
+            card["av_salary"],
+            card["downloads"],
+            card["popularity"],
+            card["job_availability"],
         )
-    
+
     def find_by_popularity(self, popularity):
         rows = self._connection.execute(
-            'SELECT * from cards WHERE popularity = %s', [popularity])
+            "SELECT * from cards WHERE popularity = %s", [popularity]
+        )
         if not rows:
             raise Exception("Popularity not listed, please try again.")
         else:
             card = rows[0]
         return Card(
-            card["id"], card["language_name"], card["age"], card["av_salary"], card["downloads"], card["popularity"], card["job_availability"]
+            card["id"],
+            card["language_name"],
+            card["age"],
+            card["av_salary"],
+            card["downloads"],
+            card["popularity"],
+            card["job_availability"],
         )
-    
+
     def find_by_job_availability(self, job_availability):
         rows = self._connection.execute(
-            'SELECT * from cards WHERE job_availability = %s', [job_availability])
+            "SELECT * from cards WHERE job_availability = %s", [job_availability]
+        )
         if not rows:
             raise Exception("Job availability not listed, please try again.")
         cards = []
         for row in rows:
-            item = Card(row["id"], row["language_name"], row["age"], row["av_salary"], row["downloads"], row["popularity"], row["job_availability"])
+            item = Card(
+                row["id"],
+                row["language_name"],
+                row["age"],
+                row["av_salary"],
+                row["downloads"],
+                row["popularity"],
+                row["job_availability"],
+            )
             cards.append(item)
         return cards
-    
+
     def update_all_job_availabilities(self):
-        api_url = self.requester.get('https://www.reed.co.uk/api/1.0/search?keywords={keywords}')
+        api_url = self.requester.get(
+            "https://www.reed.co.uk/api/1.0/search?keywords={keywords}"
+        )
         cards = self.all()
 
         for card in cards:
             programming_language = card.language_name
 
             # Make the API request to reed.co.uk
-            response = requests.get(api_url, params={
-                "keywords": programming_language
-            })
+            response = requests.get(api_url, params={"keywords": programming_language})
 
             if response.status_code == 200:
                 job_listings = response.json()["results"]
@@ -100,7 +151,7 @@ class CardRepository:
                 self.update_card_job_availability(card.id, job_availability)
             else:
                 raise Exception("Error")
-            
+
         return response.json()
 
     def update_card_job_availability(self, card_id, job_availability):
