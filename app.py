9712ecd2a4fb94749ffb4e9becb84dfee1b34de1
nish.rai <nishadrai14@gmail.com>
2023-07-06 11:54:39 +0100

    updating deck logic and top card value logic

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, request, jsonify
 from dotenv import load_dotenv
-import os
 from flask_socketio import SocketIO, emit
 from flask_cors import CORS
 from cards import cards_player_1, cards_player_2
@@ -10,42 +9,54 @@ load_dotenv()
 app = Flask(__name__)
 CORS(app, origins="http://localhost:5173")
 socketio = SocketIO(app, cors_allowed_origins="http://localhost:5173")
-clients = []
 
-
-@app.route("/")
-def index():
-    return "Hello, World!"
-
+leading_player = None
+client_usernames = []
+client_decks = {}
 
 @socketio.on("username")
 def handle_username(username):
-    if username not in clients:
-        clients.append(username)
-        if clients[0] == username:
-            emit("data", cards_player_1, to=request.sid)
-        elif clients[1] == username:
-            emit("data", cards_player_2, to=request.sid)
+    global leading_player
+    if username not in client_usernames:
+        client_usernames.append(username)
+        if client_usernames[0] == username:
+            leading_player = username
+            client_decks[username] = cards_player_1
+            emit("data", cards_player_1[0], to=request.sid)
+        elif client_usernames[1] == username:
+            client_decks[username] = cards_player_2
+            emit("data", cards_player_2[0], to=request.sid)
 
-    elif username in clients:
-        if clients[0] == username:
-            emit("data", cards_player_1, to=request.sid)
-        elif clients[1] == username:
-            emit("data", cards_player_2, to=request.sid)
+    elif username in client_usernames:
+        if client_usernames[0] == username:
+            emit("data", client_decks[username][0], to=request.sid)
+        elif client_usernames[1] == username:
+            emit("data", client_decks[username][0], to=request.sid)
 
 
 @socketio.on("disconnect")
 def handle_disconnect(username):
-    if username in clients:
-        clients.remove(username)
+    if username in client_usernames:
+        client_usernames.remove(username)
 
-
+# Attribute selected by leading player
 @socketio.on("message")
 def handle_message(data):
-    username = data.get("username")
+    # React: stop sending username?
+    losing_player = next(username for username in client_usernames if username != leading_player)
     stat = data.get("stat")
-    print(f"received message: {stat} from: {username}")
-    emit("message", data)
+    print(losing_player)
+
+    # access value of top card with given key for leading player
+    leading_top_card = client_decks[leading_player][0]
+    leading_value = leading_top_card.get("stats").get(stat)
+    
+    # access value of top card with given key for other player
+    losing_top_card = client_decks[losing_player][0]
+    losing_value = losing_top_card.get("stats").get(stat)
+    print(leading_value > losing_value)
+    
+
 
 
 if __name__ == "__main__":
--- cards.py
+++ cards.py
@@ -10,12 +10,24 @@ cards_player_1 = [
             "popularity": 99,
             "job_availability": 95,
         },
-    }
+    },
+    {
+        "id": 2,
+        "name": "Javascript 2",
+        "imageUrl": "https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png",
+        "stats": {
+            "age": 10,
+            "avg_salary": 71457,
+            "downloads": 5000000,
+            "popularity": 99,
+            "job_availability": 95,
+        },
+    },
 ]
 
 cards_player_2 = [
     {
-        "id": 2,
+        "id": 3,
         "name": "Python",
         "imageUrl": "https://upload.wikimedia.org/wikipedia/commons/c/c3/Python-logo-notext.svg",
         "stats": {
@@ -25,5 +37,17 @@ cards_player_2 = [
             "popularity": 100,
             "job_availability": 90,
         },
-    }
+    },
+    {
+        "id": 4,
+        "name": "Python 2",
+        "imageUrl": "https://upload.wikimedia.org/wikipedia/commons/c/c3/Python-logo-notext.svg",
+        "stats": {
+            "age": 30,
+            "avg_salary": 80000,
+            "downloads": 8000000,
+            "popularity": 100,
+            "job_availability": 90,
+        },
+    },
 ]
