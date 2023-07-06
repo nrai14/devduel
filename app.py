3bb8a9215cad414dcbfea28e04706c7ff941c2e6
Adrian Hards <github@ahards.com>
2023-07-06 15:21:47 +0100

    update requirements

---
+++ app.py
@@ -0,0 +1,20 @@
+import os
+from flask import Flask, request
+
+# Create a new Flask app
+app = Flask(__name__)
+
+
+# == Your Routes Here ==
+@app.route("/", methods=["GET"])
+def get_home():
+    return "hello jasmine!"
+
+
+# all renders JSON
+
+# These lines start the server if you run this file directly
+# They also start the server configured to use the test database
+# if started in test mode.
+if __name__ == "__main__":
+    app.run(debug=True, port=int(os.environ.get("PORT", 5000)))
